f95e74d40854b97eed10863b440636b35302261e
SamuelFontes <samuguel@hotmail.com.br>
2022-09-29 09:35:45 -0300

    Order: Orders CRUD

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -19,18 +19,79 @@ public class OrdersController : ControllerBase
 
     [HttpGet]
     [Route("Orders")]
-    public IActionResult GetAll()
+    public IActionResult GetAll(int tenantId)
     {
         try
         {
-            var res = _context.Orders.ToList();
+            var res = _context.Orders.Where(order => order.TenantId == tenantId).ToList();
             return Ok(res);
         }
         catch (Exception e)
         {
             return BadRequest(e.Message);
         }
-
     }
 
+    [HttpGet]
+    [Route("Order")]
+    public IActionResult GetSingle(int tenantId, int orderId)
+    {
+        try
+        {
+            var res = _context.Orders.Where(order => order.TenantId == tenantId && order.OrderId == orderId).ToList();
+            return Ok(res);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost]
+    [Route("Order")]
+    public IActionResult Create(Order model)
+    {
+        try
+        {
+            var res = _context.Add(model);
+            _context.SaveChanges();
+            return Ok(res);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPut]
+    [Route("Order")]
+    public IActionResult Update(Order model)
+    {
+        try
+        {
+            var res = _context.Update(model);
+            _context.SaveChanges();
+            return Ok(res);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpDelete]
+    [Route("Order")]
+    public IActionResult Delete(Order model)
+    {
+        try
+        {
+            var res = _context.Remove(model);
+            _context.SaveChanges();
+            return Ok(res);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
 }
